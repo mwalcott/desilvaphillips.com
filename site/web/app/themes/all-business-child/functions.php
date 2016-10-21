9bea4c96f8516376e0ccf2f57e4289a6f34e8214
mwalcott <m.walcott33@gmail.com>
2016-10-21 03:07:37 -0600

    custom widget

--- site/web/app/themes/all-business-child/functions.php
+++ site/web/app/themes/all-business-child/functions.php
@@ -34,4 +34,73 @@ function de_widgets_init() {
 
 }
 add_action( 'widgets_init', 'de_widgets_init' );
+
+
+// Creating the widget 
+class wpb_widget extends WP_Widget {
+
+function __construct() {
+	parent::__construct(
+		// Base ID of your widget
+		'wpb_widget', 
+		
+		// Widget name will appear in UI
+		__('Footer Block', 'wpb_widget_domain'), 
+		
+		// Widget description
+		array( 'description' => __( '', 'wpb_widget_domain' ), ) 
+	);
+}
+
+// Creating widget front-end
+// This is where the action happens
+public function widget( $args, $instance ) {
+	$title = apply_filters( 'widget_title', $instance['title'] );
+	// before and after widget arguments are defined by themes
+	echo $args['before_widget']; ?>
+	
+		<p><img src="/app/uploads/2016/10/logo-alt.png"/></p>
+		<p><img src="/app/uploads/2016/10/oaklins.png"/></p>
+		<ul>
+			<li class="address">
+				Main Office 475 Park Avenue South, 22nd Floor<br />
+				New York, NY 10016
+			</li>
+			<li class="phone">212.686.9700</li>	
+			<li class="mail"><a href="mailto:">Email Us</a></li>
+		</ul>	
+		<?php gravity_form( 3, false, false, false, '', false ); ?>
+	<?php echo $args['after_widget'];
+}
+		
+// Widget Backend 
+public function form( $instance ) {
+	if ( isset( $instance[ 'title' ] ) ) {
+		$title = $instance[ 'title' ];
+	}
+	else {
+		$title = __( 'New title', 'wpb_widget_domain' );
+	}
+	// Widget admin form
+?>
+<p>
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php 
+}
+	
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+	$instance = array();
+	$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+	return $instance;
+}
+} // Class wpb_widget ends here
+
+// Register and load the widget
+function wpb_load_widget() {
+	register_widget( 'wpb_widget' );
+}
+add_action( 'widgets_init', 'wpb_load_widget' );
 ?>
