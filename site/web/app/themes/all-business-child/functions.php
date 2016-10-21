76af7fc6ded4568886ed6e3b17d1240b5bb438c2
mwalcott <m.walcott33@gmail.com>
2016-10-21 13:10:39 -0600

    header adjustments

--- site/web/app/themes/all-business-child/functions.php
+++ site/web/app/themes/all-business-child/functions.php
@@ -67,7 +67,7 @@ public function widget( $args, $instance ) {
 				New York, NY 10016
 			</li>
 			<li class="phone">212.686.9700</li>	
-			<li class="mail"><a href="mailto:">Email Us</a></li>
+			<li class="mail"><a href="/contact">Email Us</a></li>
 		</ul>	
 		<?php gravity_form( 3, false, false, false, '', false ); ?>
 	<?php echo $args['after_widget'];
